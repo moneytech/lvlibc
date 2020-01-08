e102d81d6ab8f6f48371c15feb28c22989d26b04
mintsuki <mintsuki@protonmail.com>
2020-01-08 21:36:31 +0100

    Implement __stack_chk_fail

--- src/ports/qword/crt0.c
+++ src/ports/qword/crt0.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <assert.h>
 
 int main();
 
@@ -6,4 +7,8 @@ void _start(void) {
     // TODO: this is a stub.
     int ret = main();
     exit(ret);
+}
+
+void __stack_chk_fail(void) {
+    assert(!"Stack overflow detected");
 }
