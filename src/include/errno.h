1da51a87e26546fcc256f5e7052d3272ca883404
mintsuki <mintsuki@protonmail.com>
2020-01-12 18:34:48 +0100

    Add some errno stubs

---
+++ src/include/errno.h
@@ -0,0 +1,16 @@
+#ifndef __ERRNO_H__
+#define __ERRNO_H__
+
+#include <lvlibc/errno-abi.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern int errno;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ src/include/lvlibc/errno-abi.h
@@ -0,0 +1,1 @@
+../../../current_port/errno-abi.h
